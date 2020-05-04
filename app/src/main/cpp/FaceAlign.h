10ccd3ae44a5b60e0dd049ccaa2ff1138108970d
szc <charleyszc@foxmail.com>
2020-05-05 04:00:57 +0800

    update FaceAlign.h

--- app/src/main/cpp/FaceAlign.h
+++ app/src/main/cpp/FaceAlign.h
@@ -18,7 +18,7 @@
 #include <cmath>
 
 using namespace std;
-/*
+/* ???MTCNN???????5????????
 ??????points?
 0,5 ?? 1,6 ??
 2,7 ?? 3,8 ?? 4,9 ??
@@ -28,11 +28,11 @@ class FaceAlign {
 public:
 	FaceAlign();
 	~FaceAlign();
-	pair<cv::Mat, string> align(cv::Mat& img, const vector<float>& landmarks, int desired_size = 160, float padding = 0.1);	// 返回对齐的图像和相对应的位置信息
+	pair<cv::Mat, string> align(cv::Mat& img, const vector<float>& landmarks, int desired_size = 160, float padding = 0.1);	// ????????????????
 
 private:
-	string getPos(const vector<float>& points); // 用于判断是左脸右脸还是中间脸，points[0] points[1] points[2] 之间的差的比例代表左右
-	vector<float> list2colmatrix(const vector<pair<float, float>>& pts_list); // 矩阵转换
+	string getPos(const vector<float>& points); // ???????????????points[0] points[1] points[2] ???????????
+	vector<float> list2colmatrix(const vector<pair<float, float>>& pts_list); // ????
 	pair<cv::Mat, cv::Mat> find_tfrom_between_shapes(const vector<float>& from_shape, const vector<float>& to_shape);
 };
 
